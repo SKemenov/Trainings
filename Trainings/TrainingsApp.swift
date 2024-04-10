23eca2d44caf5f2291708ea126e2a168e5093e29
Sergey Kemenov <skemenov@icloud.com>
2024-04-10 20:33:27 +0300

    Initial Commit

---
+++ Trainings/TrainingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainingsApp.swift
+//  Trainings
+//
+//  Created by Sergey Kemenov on 10.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct TrainingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
